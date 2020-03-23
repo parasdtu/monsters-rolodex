b3f446cfa63454eb305e905759750aa861fa687d
PARAS <helloiamparas@gmail.com>
2020-03-23 13:08:24 +0530

    added files for monsters-rolodex

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+  constructor(){
+    super();
+
+    this.state={
+      monsters: [],
+      searchfield: ''
+    };
+  }
+
+  componentDidMount(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(response => response.json())
+    .then(users => this.setState({monsters: users}));
+  }
+  
+  render() {
+    const {monsters,searchfield }=this.state;
+    const filteredMonsters=monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchfield.toLowerCase())
+      );
+
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SearchBox 
+
+          placeholder='seacrh robots'
+          handleChange={e =>this.setState({searchfield: e.target.value})}
+
+        />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+
 }
+
 
 export default App;
---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './search-box.styles.css';
+
+export const SearchBox = ({placeholder, handleChange})  => (
+
+    <input 
+
+        className='search'
+        input='seacrh'
+        placeholder={placeholder}
+        onChange={handleChange}
+
+    />    
+
+
+);  
