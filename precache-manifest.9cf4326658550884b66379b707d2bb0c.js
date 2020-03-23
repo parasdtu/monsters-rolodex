058bf18ee9d7f589cafa5f3ac9bc23b60e3f3a23
PARAS <helloiamparas@gmail.com>
2020-03-23 13:06:58 +0530

    Updates

---
+++ precache-manifest.9cf4326658550884b66379b707d2bb0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a9154646105481b67fde74ef3b6964f0",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a3c29825fdb89da484d5",
+    "url": "/monsters-rolodex/static/css/main.a562a15c.chunk.css"
+  },
+  {
+    "revision": "d75db5fa38bb8011da20",
+    "url": "/monsters-rolodex/static/js/2.7f3818e9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7f3818e9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3c29825fdb89da484d5",
+    "url": "/monsters-rolodex/static/js/main.0a057645.chunk.js"
+  },
+  {
+    "revision": "886ea7a70bd4dd10bad3",
+    "url": "/monsters-rolodex/static/js/runtime-main.a84bbd48.js"
+  }
+]);
